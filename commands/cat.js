8bf0dcec34fdbe1a923e040a56375ea9f541f8d4
Bossaidy <47262368+Bossaidy@users.noreply.github.com>
2019-06-29 23:33:47 -0400

    I like cats

---
+++ commands/cat.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js'); 
+const get = require('superagent'); 
+
+exports.run = async (client, message) => { 
+  await get('http://aws.random.cat/meow').then(res => { 
+  const DogEm = new Discord.RichEmbed()
+    const CatEm = new Discord.RichEmbed()
+            .setImage(res.body.file)
+            .setColor("#E6E6FF")
+            .setAuthor('Meow! Kitty incoming!) 
+  
+    return message.channel.send(CatEm) 
+  });
+};
