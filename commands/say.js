f268ef23cb5339c39b5044a3d76cdce341db2083
Bossaidy <47262368+Bossaidy@users.noreply.github.com>
2019-06-29 22:24:50 -0400

    Owner only say.js

--- commands/say.js
+++ commands/say.js
@@ -2,6 +2,8 @@ const discord = require('discord.js');
 const config = require('../config.json'); 
 const client = require('../index.js'); 
 
+if(!client.config.owners.includes(message.author.id)) return;
+
 exports.run = async (client, message, args) => { 
 
 message.channel.send(args.join(' ')); 
