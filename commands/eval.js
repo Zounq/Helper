31bad8fa7f5d78b3b82688a3b8482230c1f0a59c
Bossaidy <47262368+Bossaidy@users.noreply.github.com>
2019-06-29 22:00:27 -0400

    Added eval

---
+++ commands/eval.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const Client = require('../index.js')
+exports.run = async (client, message) => {
+
+if(message.author.id !== config.owners) return;
+
+    let evaled;
+    try {
+        evaled = await eval(args.join(' '));
+    } catch (err) {
+        const embed = new Discord.RichEmbed();
+        embed.setTitle('JavaScript Eval');
+        embed.setColor('RED');
+        embed.setDescription(`[Error]\n \`\`\`js\n${err}\`\`\``);
+        embed.setFooter(Client.user.username, Client.user.avatarURL);
+        embed.setTimestamp();
+        return message.channel.send(embed);
+    }
+
+    if (typeof evaled === 'string') {
+        evaled = evaled.replace(client.token, '[TOKEN]');
+    }
+
+
+    if (typeof evaled === 'object') {
+        evaled = require('util').inspect(evaled, {
+            depth: 0
+        });
+    }
+    if (evaled == undefined) {
+        evaled = 'undefined';
+    }
+
+    const embed = new Discord.RichEmbed();
+    embed.setTitle('JavaScript Eval');
+    embed.setColor('GREEN');
+    embed.setDescription(`\`\`\`js\n${evaled}\`\`\``);
+    embed.setFooter(client.user.username, client.user.avatarURL);
+    embed.setTimestamp();
+    message.channel.send(embed);
+};
