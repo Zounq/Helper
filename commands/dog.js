e3de6a852cc154099474b2e81c7fbbef5869d7d7
Bossaidy <47262368+Bossaidy@users.noreply.github.com>
2019-06-29 23:32:22 -0400

    Dogs are hot

---
+++ commands/dog.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js'); 
+const get = require('superagent'); 
+
+exports.run = async (client, message) => { 
+  await get('https://nekos.life/api/v2/img/woof;).then(res => { 
+  const DogEm = new Discord.RichEmbed()
+  .setImage(res.body.url) 
+  .setColor('RANDOM') 
+  .setAuthor('Woof Woof, Doggo incoming!) 
+  
+    return message.channel.send(DogEm) 
+  });
+};
