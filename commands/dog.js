ac0713f2ad549d12dfaecec6d5ce856fc61cc317
Bossaidy <47262368+Bossaidy@users.noreply.github.com>
2019-06-29 23:34:01 -0400

    Keyboard smasher 101

--- commands/dog.js
+++ commands/dog.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const get = require('superagent'); 
 
 exports.run = async (client, message) => { 
-  await get('https://nekos.life/api/v2/img/woof;).then(res => { 
+  await get('https://nekos.life/api/v2/img/woof').then(res => { 
   const DogEm = new Discord.RichEmbed()
   .setImage(res.body.url) 
   .setColor('RANDOM') 
