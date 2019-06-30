11979180411c737878876cddd9cd64540e8815b8
Bossaidy <47262368+Bossaidy@users.noreply.github.com>
2019-06-30 12:24:34 -0400

    Nice command for mason

---
+++ commands/mason.js
@@ -0,0 +1,15 @@
+const discord = require('discord.js'); 
+const config = require('../config.json'); 
+const client = require('../index.js'); 
+
+exports.run = async (client, message, args) => { 
+  
+  let mason = new discord.RichEmbed 
+  
+  .setTimestamp()
+  .setImage('https://images-ext-1.discordapp.net/external/fUTJ5LhI4xfwwzqurbrYnuGnJ11iyQsQQMGhqaFk2qA/%3Fsize%3D128/https/cdn.discordapp.com/avatars/358776042829119498/a_28596ca6e0d05d4872ca54eae0ed49b6.gif') 
+  .setTitle('Mason is some yummy fried chicken!! 😋😋
+  
+    message.channel.send({mason})
+  
+  
