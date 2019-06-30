91a477e4954ace7f88e3e2f8f8f4cfd8078d1d47
Bossaidy <47262368+Bossaidy@users.noreply.github.com>
2019-06-30 12:34:33 -0400

    mason is cool

--- commands/mason.js
+++ commands/mason.js
@@ -10,6 +10,6 @@ exports.run = async (client, message, args) => {
   .setImage('https://images-ext-1.discordapp.net/external/fUTJ5LhI4xfwwzqurbrYnuGnJ11iyQsQQMGhqaFk2qA/%3Fsize%3D128/https/cdn.discordapp.com/avatars/358776042829119498/a_28596ca6e0d05d4872ca54eae0ed49b6.gif') 
   .setTitle('Mason is some yummy fried chicken!! 😋😋
   
-    return message.channel.send({mason})
+    return message.channel.send(mason)
   
   
