fb90e2414d7a3f52457f9f7fc009a0e92ad5d84e
Bossaidy <47262368+Bossaidy@users.noreply.github.com>
2019-06-29 22:28:00 -0400

    I suck hard (╯°□°）╯︵ ┻━┻

--- commands/say.js
+++ commands/say.js
@@ -2,9 +2,8 @@ const discord = require('discord.js');
 const config = require('../config.json'); 
 const client = require('../index.js'); 
 
-if(!client.config.owners.includes(message.author.id)) return;
-
 exports.run = async (client, message, args) => { 
+  if(!client.config.owners.includes(message.author.id)) return;
 
 message.channel.send(args.join(' ')); 
 message.delete()
