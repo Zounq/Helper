161f8561d8d68db5084bb33d12295c72b88affb0
Bossaidy <47262368+Bossaidy@users.noreply.github.com>
2019-07-01 10:36:48 -0400

    Finally

---
+++ commands/channelbot.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js'); 
+const get = require('superagent'); 
+
+exports.run = async (client, message) => { 
+
+    message.channel.send('<:channelbot:575142890578378752> **ChannelBot**\n\n**Invite Link:** <https://bit.ly/invitecb>\n**Website:** <https://channelbot.glitch.me>\n**Documentation:** <https://channelbot.gitbook.io/docs/>\n**Purchasing Premium:** <https://www.patreon.com/ChannelBot>\n**Discord Server Invite:** https://discordapp.com/invite/vB47a5r')
+}
