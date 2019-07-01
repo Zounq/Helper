5c17409f0bdbee4e0622c64efaae0a6c08399e52
Bossaidy <47262368+Bossaidy@users.noreply.github.com>
2019-06-30 21:20:03 -0400

    Mason is one nice chicken

--- commands/upvote.js
+++ commands/upvote.js
@@ -4,5 +4,12 @@ const client = require('../index.js');
 
 exports.run = async (client, message) => { 
 
-message.channel.send('Want to upvote ChannelBot? Well click here <https://discordbots.org/bot/443545183997657120/vote>.\nUpvoting is a frew way to support ChannelBot and Codingpro! If you decide the upvote, you will get the <@&594559964115238913> role, as well as some more <@356950275044671499> money!\n**You can upvote every 12 hours**')
+const upvoteembed = new Discord.RichEmbed()
+.setDescription("Want to upvote ChannelBot? Well click [here!](https://discordbots.org/bot/443545183997657120/vote)
+Upvoting is a frew way to support ChannelBot and Codingpro! If you decide the upvote, you will get the @Upvoted role, as well as some more @UnbelievaBoat money!\n**You can upvote every 12 hours**")
+.setFooter("Thank you for upvoting!")
+.setColor("GREEN")
+.setTimestamp()
+
+message.channel.send(upvoteembed)
 }
