76d466e6d24b491ad1c05bf9290aef31aedeaf32
Bossaidy <47262368+Bossaidy@users.noreply.github.com>
2019-07-01 10:32:24 -0400

    I like upvoting channelbot

--- commands/upvote.js
+++ commands/upvote.js
@@ -4,9 +4,8 @@ const client = require('../index.js');
 
 exports.run = async (client, message) => { 
 
-const upvoteembed = new Discord.RichEmbed()
-.setDescription("Want to upvote ChannelBot? Well click [here!](https://discordbots.org/bot/443545183997657120/vote)
-Upvoting is a frew way to support ChannelBot and Codingpro! If you decide the upvote, you will get the @Upvoted role, as well as some more @UnbelievaBoat money!\n**You can upvote every 12 hours**")
+const upvoteembed = new discord.RichEmbed()
+.setDescription("Want to upvote ChannelBot? Well click [here!](https://discordbots.org/bot/443545183997657120/vote) Upvoting is a frew way to support ChannelBot and Codingpro! If you decide the upvote, you will get the <@&594559964115238913> role, as well as some more <@356950275044671499> money!\n**You can upvote every 12 hours**")
 .setFooter("Thank you for upvoting!")
 .setColor("GREEN")
 .setTimestamp()
